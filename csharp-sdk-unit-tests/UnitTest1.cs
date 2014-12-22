94ec9d01f38e5fc3e322764e15d01eb844489287
Peter <peterdwersteg@gmail.com>
2014-12-22 12:26:42 +0100

    Added a unit-testing project

---
+++ csharp-sdk-unit-tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace csharp_sdk_unit_tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
